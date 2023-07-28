897beb3db2bc37216af182a9eb68e408dcb94d29
Unain <jvdstoel@pm.me>
2023-07-28 16:53:17 +0200

    Added Create Article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,20 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreArticleRequest;
 use App\Models\Article;
+use App\Models\Category;
+use App\Models\Tag;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 
 class ArticleController extends Controller
 {
     public function index(): View
     {
-        return view('blog.index', [
+        return view('article.index', [
             'articles' => Article::with('category', 'tags')->latest()->simplePaginate(10),
         ]);
     }
 
     public function show(Article $article): View
     {
-        return view('blog.show', compact('article'));
+        return view('article.show', compact('article'));
+    }
+
+    public function create(): View
+    {
+        $categories = Category::all();
+        $tags = Tag::all();
+
+        return view('article.create', compact('categories', 'tags'));
+    }
+
+    public function store(StoreArticleRequest $request): RedirectResponse
+    {
+        if ($request->has('tags')) {
+            $tags = $request->get('tags');
+            dd($tags);
+
+            foreach ($tags as $tag) {
+
+            }
+        }
+
+        $article = Article::create($request->validated());
+        // $article->tags()->attach($tags);
+
+        return redirect()->route('article.show', $article);
+    }
+
+    public function edit(Article $article): View
+    {
+        return view('article.edit', compact('article'));
+    }
+
+    public function update(StoreArticleRequest $request, Article $article): RedirectResponse
+    {
+        $article->update($request->validated());
+
+        return redirect()->route('article.show', $article);
+    }
+
+    public function destroy(Article $article): RedirectResponse
+    {
+        $article->delete();
+
+        return redirect()->route('article.index');
     }
 }
---
+++ resources/views/article/create.blade.php
@@ -0,0 +1,59 @@
+<x-main-layout>
+    <h2 class="text-3xl font-extrabold dark:text-gray-50">New Article</h2>
+    <form method="POST" action="{{ route('article.store') }}">
+        @csrf
+        <div class="mb-6 space-y-4">
+            <div>
+                <label for="title" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                <input type="text" name="title" id="title"
+                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-violet-500 focus:ring-violet-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-violet-500 dark:focus:ring-violet-500"
+                    placeholder="Title for the article" required>
+                @error('title')
+                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                @enderror
+            </div>
+            <div>
+                <label for="category_id"
+                    class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                <select name="category_id" id="category_id"
+                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-violet-500 focus:ring-violet-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-violet-500 dark:focus:ring-violet-500"
+                    required>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    @endforeach
+                </select>
+                @error('category_id')
+                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                @enderror
+            </div>
+            <div>
+                <label for="text" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Text</label>
+                <textarea id="text" name="text" placeholder="Write something..." rows="12"
+                    class="w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-violet-500 focus:ring-violet-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-violet-500 dark:focus:ring-violet-500"></textarea>
+                @error('text')
+                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                @enderror
+            </div>
+            <div>
+                <label for="tags" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Tags</label>
+                <select name="tags[]" id="tags" multiple
+                    class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-violet-500 focus:ring-violet-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-violet-500 dark:focus:ring-violet-500"
+                    required>
+                    @foreach ($tags as $tag)
+                        <option value="{{ $tag->id }}">{{ $tag->name }}</option>
+                    @endforeach
+                </select>
+                @error('tags')
+                    <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
+                @enderror
+            </div>
+        </div>
+        <button type="submit"
+            class="w-full rounded-lg bg-violet-400 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-violet-800 focus:outline-none focus:ring-4 focus:ring-violet-300 dark:bg-violet-400 dark:hover:bg-violet-700 dark:focus:ring-violet-800 sm:w-auto">Submit</button>
+    </form>
+
+    @if ($errors->isNotEmpty())
+        {{ $errors }}
+    @endif
+
+</x-main-layout>
