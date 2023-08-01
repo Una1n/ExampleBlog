bf1062ed67736498e717ec620e43dbfad431f1db
Unain <jvdstoel@pm.me>
2023-08-01 18:03:50 +0200

    Added old values to forms when form errors

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -7,7 +7,7 @@
             <label for="name" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">Name</label>
             <input type="text" name="name" id="name"
                 class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-violet-500 focus:ring-violet-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-violet-500 dark:focus:ring-violet-500"
-                placeholder="name" required value="{{ $category->name }}">
+                placeholder="name" required value="{{ old('name', $category->name) }}">
             @error('name')
                 <p class="mt-2 text-sm text-red-600 dark:text-red-500">{{ $message }}</p>
             @enderror
